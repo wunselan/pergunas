edfeae1841408b5dbdeb017d96ad357b08080dd0
Wunsel Arto Negoro <wunselan@gmail.com>
2020-02-28 00:05:57 +0700

    mengubah view struktur organisasi jadi bisa collapse

--- application/views/organisasi.php
+++ application/views/organisasi.php
@@ -32,66 +32,111 @@
             <div class="row">
                 <div class="col-md-12 text-center">
                     <div class="mu-service-area">
-                        <div class="accordion" id="accordionExample">
+                        
                             <div class="row">
-
+                                
                                 <div class="col-md-6">
-                                    <div class="card text-center">
-                                        <div class="card-header">
-                                            Kepala Sekolah
-                                        </div>
-                                        <div class="card-body">
-                                            <img style="max-width:30%; margin-bottom: 10px;" src="/pergunas/application/assets/images/pergunas5.jpg" alt="img">
-                                            <h5 class="card-title">Special title treatment</h5>
+                                    <div class="accordion" id="accordionExample">
+                                        <div class="card text-center">
+                                            <div class="card-header" id="headingOne">
+                                                <a class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">Kepala Sekolah</a>
+                                            </div>
+                                            <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+                                                <div class="card-body">
+                                                    <img style="max-width:30%; margin-bottom: 10px;" src="/pergunas/application/assets/images/pergunas5.jpg" alt="img">
+                                                    <h5 class="card-title">Special title treatment</h5>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
 
                                 <div class="col-md-6">
-                                    <div class="card text-center">
-                                        <div class="card-header">
-                                            Wakil Kepala Sekolah
-                                        </div>
-                                        <div class="card-body">
-                                            <img style="max-width:30%; margin-bottom: 10px;" src="/pergunas/application/assets/images/pergunas5.jpg" alt="img">
-                                            <h5 class="card-title">Special title treatment</h5>
+                                    <div class="accordion" id="accordionExample2">
+                                        <div class="card text-center">
+                                            <div class="card-header" id="headingTwo">
+                                                <a class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">Wakil Kepala Sekolah</a>
+                                            </div>
+                                            <div id="collapseTwo" class="collapse show" aria-labelledby="headingTwo" data-parent="#accordionExample2">
+                                                <div class="card-body">
+                                                    <img style="max-width:30%; margin-bottom: 10px;" src="/pergunas/application/assets/images/pergunas5.jpg" alt="img">
+                                                    <h5 class="card-title">Special title treatment</h5>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-
                             </div>
 
                             <div class="row">
                                 <div class="col-md-12">
                                     <div class="card guru text-center">
-                                        <div class="card-header">
-                                            Guru
+                                        <div class="accordion" id="accordionExample3">
+                                            <div class="card-header">
+                                                <a class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">Guru</a>
+                                            </div>
+                                            <div id="collapseThree" class="collapse show" aria-labelledby="headingThree" data-parent="#accordionExample3">
+                                                <div class="row">
+                                                
+                                                    <div class="card-body col-md-3">
+                                                        <img style="max-width:30%; margin-bottom: 10px;" src="/pergunas/application/assets/images/pergunas5.jpg" alt="img">
+                                                        <h5 class="card-title">Special title treatment</h5>
+                                                    </div>
+                                                    <div class="card-body col-md-3">
+                                                        <img style="max-width:30%; margin-bottom: 10px;" src="/pergunas/application/assets/images/pergunas5.jpg" alt="img">
+                                                        <h5 class="card-title">Special title treatment</h5>
+                                                    </div>
+                                                    <div class="card-body col-md-3">
+                                                        <img style="max-width:30%; margin-bottom: 10px;" src="/pergunas/application/assets/images/pergunas5.jpg" alt="img">
+                                                        <h5 class="card-title">Special title treatment</h5>
+                                                    </div>
+                                                    <div class="card-body col-md-3">
+                                                        <img style="max-width:30%; margin-bottom: 10px;" src="/pergunas/application/assets/images/pergunas5.jpg" alt="img">
+                                                        <h5 class="card-title">Special title treatment</h5>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </div>
-
-                                        <div class="row">
-                                            <div class="card-body col-md-3">
-                                                <img style="max-width:30%; margin-bottom: 10px;" src="/pergunas/application/assets/images/pergunas5.jpg" alt="img">
-                                                <h5 class="card-title">Special title treatment</h5>
-                                            </div>
-                                            <div class="card-body col-md-3">
-                                                <img style="max-width:30%; margin-bottom: 10px;" src="/pergunas/application/assets/images/pergunas5.jpg" alt="img">
-                                                <h5 class="card-title">Special title treatment</h5>
-                                            </div>
-                                            <div class="card-body col-md-3">
-                                                <img style="max-width:30%; margin-bottom: 10px;" src="/pergunas/application/assets/images/pergunas5.jpg" alt="img">
-                                                <h5 class="card-title">Special title treatment</h5>
-                                            </div>
-                                            <div class="card-body col-md-3">
-                                                <img style="max-width:30%; margin-bottom: 10px;" src="/pergunas/application/assets/images/pergunas5.jpg" alt="img">
-                                                <h5 class="card-title">Special title treatment</h5>
-                                            </div>
-                                        </div>
-
                                         
                                     </div>
                                 </div>
                             </div>
-                        </div>
+
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="card guru text-center">
+                                        <div class="accordion" id="accordionExample4">
+                                            <div class="card-header">
+                                                <a class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">Staff</a>
+                                            </div>
+                                            <div id="collapseFour" class="collapse show" aria-labelledby="headingFour" data-parent="#accordionExample4">
+                                                <div class="row">
+                                                
+                                                    <div class="card-body col-md-3">
+                                                        <img style="max-width:30%; margin-bottom: 10px;" src="/pergunas/application/assets/images/pergunas5.jpg" alt="img">
+                                                        <h5 class="card-title">Special title treatment</h5>
+                                                    </div>
+                                                    <div class="card-body col-md-3">
+                                                        <img style="max-width:30%; margin-bottom: 10px;" src="/pergunas/application/assets/images/pergunas5.jpg" alt="img">
+                                                        <h5 class="card-title">Special title treatment</h5>
+                                                    </div>
+                                                    <div class="card-body col-md-3">
+                                                        <img style="max-width:30%; margin-bottom: 10px;" src="/pergunas/application/assets/images/pergunas5.jpg" alt="img">
+                                                        <h5 class="card-title">Special title treatment</h5>
+                                                    </div>
+                                                    <div class="card-body col-md-3">
+                                                        <img style="max-width:30%; margin-bottom: 10px;" src="/pergunas/application/assets/images/pergunas5.jpg" alt="img">
+                                                        <h5 class="card-title">Special title treatment</h5>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        
+                                    </div>
+                                </div>
+                            </div>
+
+                        
                     </div>
                 </div>
             </div>
