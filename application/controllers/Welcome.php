f11c2434527003ae70171a75de81fd9b9a9fb518
Wunsel Arto Negoro <wunselan@gmail.com>
2020-02-27 18:45:34 +0700

    menambahkan tampilan untuk struktur organisasi

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -48,4 +48,5 @@ class Welcome extends CI_Controller {
 		$this->load->view('organisasi');
 		$this->load->view('footer');
 	}
+	
 }
---
+++ application/views/organisasi.php
@@ -0,0 +1,92 @@
+<body>
+    <div id="mu-page-header">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div id="pot" class="mu-page-header-area">
+                        <h3 style="text-align:center; color:white;">Struktur Organisasi</h3>
+                        <h1 style="text-align:center; color:white;">SMP & SMA Perguruan Nasional</h1>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+    <section id="mu-service">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <div class="mu-service-area">
+                        <!-- Start Service Content -->
+                        <div class="row">
+                            <div class="col-md-12 text-center">
+                                <div class="mu-service-content">
+                                    <div class="row">
+                                        <!-- Start single service -->
+                                        <div class="col-md-6 text-center">
+                                            <div class="mu-single-service">
+                                                <div class="mu-single-service-content">
+                                                    <h3>Kepala Sekolah</h3>
+														<img style="max-width:25%" src="/pergunas/application/assets/images/pergunas5.jpg" alt="img">
+                                                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor commodo .</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <!-- End single service -->
+                                        <!-- Start single service -->
+                                        <div class="col-md-6 text-center">
+                                            <div class="mu-single-service">
+                                                <div class="mu-single-service-content">
+                                                    <h3>Wakil Kepala Sekolah</h3>
+                                                    <img style="max-width:25%" src="/pergunas/application/assets/images/pergunas5.jpg" alt="img">
+                                                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor commodo .</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <!-- End single service -->
+                                        <!-- Start single service -->
+                                        <div class="col-md-4 text-center">
+                                            <div class="mu-single-service">
+                                                <div class="mu-single-service-content">
+                                                    <h3>Guru</h3>
+                                                    <img style="max-width:25%" src="/pergunas/application/assets/images/pergunas5.jpg" alt="img">
+                                                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor commodo .</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <!-- End single service -->
+                                        <!-- Start single service -->
+                                        <div class="col-md-4 text-center">
+                                            <div class="mu-single-service">
+                                                <div class="mu-single-service-content">
+                                                    <h3>Guru</h3>
+                                                    <img style="max-width:25%" src="/pergunas/application/assets/images/pergunas5.jpg" alt="img">
+                                                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor commodo .</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <!-- End single service -->
+                                        <!-- Start single service -->
+                                        <div class="col-md-4 text-center">
+                                            <div class="mu-single-service">
+                                                <div class="mu-single-service-content">
+                                                    <h3>Guru</h3>
+                                                    <img style="max-width:25%" src="/pergunas/application/assets/images/pergunas5.jpg" alt="img">
+                                                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor commodo .</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <!-- End single service -->
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- End Service Content -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End Services -->	
+</body>
